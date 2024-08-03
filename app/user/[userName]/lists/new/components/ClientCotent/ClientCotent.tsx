a28b44262646c30a947909863b2b0f9acbb6db86
isaac <isaaccastrocisneros@gmail.com>
2024-08-03 14:23:48 -0500

    last

--- app/user/[userName]/lists/new/components/ClientCotent/ClientCotent.tsx
+++ app/user/[userName]/lists/new/components/ClientCotent/ClientCotent.tsx
@@ -10,6 +10,7 @@ import Searcher from './components/Searcher'
 import listItem from './interfaces/listItem'
 import Separator from '@/components/Separator'
 import Label from '@/components/Label'
+import Button from '@/components/Button'
 
 interface props
 {
@@ -40,31 +41,44 @@ export default function ClientCotent({user}:props)
 
   return (
     <>
-      <h1 className='text-[28px] text-text4 font-medium'>
-        Editing your list
-      </h1>
-      <Separator className='my-[.3rem]'/>
-      <div className='mb-[2rem]'>
-        <form id='list' action="" onSubmit={submittingForm} className="mb-[1rem]">
-          <LabedInput label="List Name" props={{ input: { name: "name" },container:{className:"mb-[.3rem]"} }} />
+      <h1 className="text-[28px] text-text4 font-medium">Editing your list</h1>
+      <Separator className="my-[.3rem]" />
+      <div className="mb-[2rem]">
+        <form
+          id="list"
+          action=""
+          onSubmit={submittingForm}
+          className="mb-[1rem]"
+        >
+          <LabedInput
+            label="List Name"
+            props={{
+              input: { name: "name" },
+              container: { className: "mb-[.3rem]" },
+            }}
+          />
           <LabedTextarea
             label="Description"
             props={{ textarea: { name: "description" } }}
           />
         </form>
         <Label>Add a game</Label>
-        <div className='max-w-[20rem]'>
-        <Searcher setList={setList} list={list} />
-        <button form='list' /* onClick={handleUpdatingList} */>lel</button>
+        <div className="max-w-[100%] justify-between flex">
+          <div className="max-w-[20rem] flex-1">
+            <Searcher setList={setList} list={list} />
+          </div>
+          <Button form="list">Create List</Button>
         </div>
       </div>
-      <div className='grid grid-cols-[repeat(5,1fr)] gap-[.7rem] mob1:grid-cols-[repeat(3,1fr)]'>
-        {
-          list.map(item=>
-            (
-              <Card handleDeleteItemList={handleDeleteItemList} key={item.listId} imgUrl={item.cover} {...item}/>
-            ))
-        }
+      <div className="grid grid-cols-[repeat(5,1fr)] gap-[.7rem] mob1:grid-cols-[repeat(3,1fr)]">
+        {list.map((item) => (
+          <Card
+            handleDeleteItemList={handleDeleteItemList}
+            key={item.listId}
+            imgUrl={item.cover}
+            {...item}
+          />
+        ))}
       </div>
     </>
   );
--- app/user/[userName]/lists/new/server/updatingList.ts
+++ app/user/[userName]/lists/new/server/updatingList.ts
@@ -12,14 +12,18 @@ interface props
     list:Array<listItem>
 }
 
+
 export default async function updatingList({list,user_id,...props} :props):Promise<string> 
 {
    try
    {
     const [results] =await pool.query<Array<RowDataPacket>>("insert into gameList set ?",{...props,user_id})
-    const rowId=results.insertId
+    const listResults=results as any 
+    const myArr = list.map(item=>[item.id,user_id])
 
-    await pool.query<Array<RowDataPacket>>("INSERT IGNORE INTO game (game_id, user_id) values ?",[list.map(item=>[item.id,user_id])])
+    await pool.query<Array<RowDataPacket>>("INSERT IGNORE INTO game (game_id, user_id) values ?",[myArr])
+    const [gamesJustAdded]=await pool.query<Array<RowDataPacket>>("select id from game where (game_id, user_id) in (?)",[myArr])
+    await  pool.query<Array<RowDataPacket>>("insert into gameListItem (game_id, list_id) values ?",[gamesJustAdded.map(game=>[game.id,listResults.insertId])])
 /* 
     const lisIds= list.map(item=>[item.id,rowId,user_id])
     await pool.query<Array<RowDataPacket>>("insert into game (game_id, list_id, user_id) values ? on duplicate key update list_id=values(list_id)",[lisIds]) */
