d3ca04008edd18202685645aa9075ea3c5fcf0d1
isaac <isaaccastrocisneros@gmail.com>
2024-12-04 21:58:06 -0500

    last

--- config/db.ts
+++ config/db.ts
@@ -2,12 +2,22 @@ import {createPool} from 'mysql2/promise';
 
 const pool = createPool(
     {
+        host:'junction.proxy.rlwy.ne',
+        user:'root',
+        password:'UaRFWktUQCjmrrmDhIVtyPnIujBzOwlG',
+        port:24065,
+        database:'backloggd'
+    }
+)
+
+/* const pool = createPool(
+    {
         host:'localhost',
         user:'root',
         password:'root123',
         port:3306,
         database:'backloggd'
     }
-)
+) */
 
 export default pool
