31e240d34bc72c25a923b39f2507e86bdce2aea2
isaac <isaaccastrocisneros@gmail.com>
2024-12-05 10:58:36 -0500

    last

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,9 @@ import get from "@/server/get";
 
 export default async function Home() {
 
-  const {res}= JSON.parse( await get({query:"select * from users"})) 
 
   return (
     <article className="mt-[4rem]">
-      
-        <span className="text-[2rem] text-[#fff]">
-          {
-          res[0].username
-          }
-        </span>
-  
       <h1 className="text-[#fff] text-[6rem] font-bold">Backloggd</h1>
       <span className="text-text4 text-[32px] font-medium">
         Discover, collect, analyze your games
