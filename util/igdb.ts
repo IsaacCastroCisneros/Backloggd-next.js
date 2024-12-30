c22f2b7e77964e6b29922bd6b97e6d0e2b452542
isaac <isaaccastrocisneros@gmail.com>
2024-12-30 11:34:26 -0500

    lol

--- util/igdb.ts
+++ util/igdb.ts
@@ -17,7 +17,7 @@ export default async function igdb({type,query=""}:props):Promise<{res:Array<any
      {
         'Accept': 'application/json',
         'Client-ID' : 'h8j1a9qs3atk4blop7oiz3yvouv5py',
-        'Authorization' :  'Bearer oqcdbxn2onu4kf408f7pwep2wja8f2', 
+        'Authorization' :  'Bearer i7hi10f7lxb7cez73yxfeeqed7b1sm', 
      }
    }
    try
